c27341f444c1c74fafc97debed50c15b33fa284e
gladys <achandogladys6@gmail.com>
2025-06-15 06:35:19 +0300

    add new changes to index.js

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 // write your code here
-// select elements
 
+document.addEventListener("DOMContentLoaded",()=>{
+  let form=document.getElementById('form');
+
+  let inputText=document.getElementById('text');
+
+  let category=document.getElementById('GuestCategory');
+
+  let guestlist=document.getElementById('list');
+
+  let toggleButton=document.getElementById('theme-toggle');
+
+  let currentTheme=localStorage.getItem('theme');
+  if(currentTheme){
+    document.body.classList.add(currentTheme);
+  }
+  toggleButton.addEventListener('click',() =>{
+    document.body.classList.toggle('dark-theme');
+    localStorage.setItem('theme',theme)
+  });
+
+  const guest
+
+
+})
 
 
